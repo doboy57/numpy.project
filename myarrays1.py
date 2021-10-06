2affbdfac6da610399eeae50d96872add30dd1ea
ryan do <ryan_do1@baylor.edu>
2021-10-06 15:45:19 -0500

    day 1

---
+++ myarrays1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random
+
+
+arr01 = np.array([[1, 2, 3], [4, 5, 6]])  # two rows and three columns
+
+arr02 = np.array([0.0, 0.1, 0.2, 0.3, 0.4])
+
+'''
+for row in arr01:
+    print(row)
+    input()
+    for col in row:
+        print(col, end=" ")
+    print()
+
+for i in arr01.flat:
+    print(i)
+'''
+
+arr03 = np.zeros(5)
+
+arro4 = np.ones((2, 4), dtype=int)
+arr05 = np.full((3, 5), 13)
+
+print()
+
+
---
+++ randomarray.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+
+
+b = np.array(np.random.randint(1, 10, size=(2, 5)))
+
+arr06 = np.arange(5)
+
+arr07 = np.arange(5, 10)
+
+arr08 = np.arange(10,1,-2)
+print()
